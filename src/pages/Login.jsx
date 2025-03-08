b46b0195a5c87c61a95384e68f894d061f74e85e
lRomanti01 <romanti159@gmail.com>
2025-03-07 20:31:45 -0500

    add googleIcon

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useUserContext } from "../context/UserContext";
 import { useRedirectActiveUser } from "../hooks/useRedirectActiveUser";
 import { auth, provider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
+import googleIcon from "../assets/googleIcon.png"
 
 function Login() {
   const { user } = useUserContext();
@@ -15,17 +16,15 @@ function Login() {
     <main className="min-h-screen bg-slate-400 flex justify-center items-center ">
       <div className="absolute w-full h-full bg-[url('./assets/Montains.jpg')] bg-center bg-cover bg-no-repeat backdrop-blur-md blur-sm"></div>
       <div className="bg-slate-100 bg-opacity-30 z-10 p-12 md:p-28 flex justify-center items-center flex-col gap-10 rounded-2xl hover:scale-110 transition-all duration-500 shadow-lg hover:shadow-black">
-        <h1 className="text-center text-5xl uppercase font-bold text-black">Sign In</h1>
+        <h1 className="text-center text-5xl uppercase font-bold text-black">
+          Sign In
+        </h1>
         <button
           onClick={handleClick}
           className="bg-[#558aed] font-bold rounded-md p-1 text-white hover:bg-[#5b89ff]"
         >
           <div className="flex justify-between items-center rounded-full">
-            <img
-              src="/src/assets/googleIcon.png"
-              alt="Google Icon"
-              className="h-12 mr-1"
-            />
+            <img src={googleIcon} alt="Google Icon" className="h-12 mr-1" />
             <p className="px-3">Sign In With Google</p>
           </div>
         </button>
